546686f37c95862ad3d98501c9557006121b2acd
Obscurexc <ricardo.a.barbosa@outlook.com>
2018-04-20 14:55:13 -0300

    versao 1.0.1

--- server.js
+++ server.js
@@ -13,16 +13,26 @@
  var express = require('express');
  var app = express();
  var bodyParse = require('body-parser');
+ var mogoose = require('mongoose');
+ var Produto = require('./app/models/produto');
 
- //Configuração da variavel app para usar o "bodyParser()"
- app.use(bodyParse.urlencoded({extended: true }));
- app.use(bodyParse.json());
 
- //Definição da Porta onde será executado nossa api
- var port = process.env.port || 8000;
+//URI: do mlba
+ mongoose.connect('mongodb://<rAugusto>:<admin>@ds056789.mlab.com:56789/node-crud-api');
 
- //Criando uma instancia das routas via Express
- var router = express.Router();
+// maneira mongoDB
+// mogoose.connect('mongodb://localhost/node-crud-api');
+
+
+//Configuração da variavel app para usar o "bodyParser()"
+app.use(bodyParse.urlencoded({extended: true }));
+app.use(bodyParse.json());
+
+//Definição da Porta onde será executado nossa api
+var port = process.env.port || 8000;
+
+//Criando uma instancia das routas via Express
+var router = express.Router();
 
 // criando uma rota de exemplo
 router.get('/',function(req,res){
